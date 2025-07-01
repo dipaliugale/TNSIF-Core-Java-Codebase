f808bdd9c1d87291e47f1b1e98e72f67cf9dd6a6
dipaliugale <ugaledipali969@gmail.com>
2025-07-01 11:36:38 +0530

    day3

---
+++ TNSIF_Training/src/tnsif/daytwo/jumpDemo/BreakDemo.java
@@ -0,0 +1,19 @@
+package tnsif.daytwo.jumpDemo;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=5; i<10; i++)
+		{
+			if(i==5)
+				//break;
+			System.out.println(i);
+			System.out.println("Hello World");
+			break;
+		}
+
+	}
+
+}
