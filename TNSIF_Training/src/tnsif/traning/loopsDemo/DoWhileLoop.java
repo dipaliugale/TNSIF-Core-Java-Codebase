4d1935f77dd85a378e724b961f8eabdc895f2417
dipaliugale <ugaledipali969@gmail.com>
2025-07-01 11:17:12 +0530

    day2

---
+++ TNSIF_Training/src/tnsif/traning/loopsDemo/DoWhileLoop.java
@@ -0,0 +1,17 @@
+package tnsif.traning.loopsDemo;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 11;
+		do {
+			System.out.println("Value of i: "+i);
+			i++;
+		}
+		while(i <= 10);
+
+	}
+
+}
---
+++ TNSIF_Training/src/tnsif/traning/loopsDemo/WhileDemo.java
@@ -0,0 +1,13 @@
+package tnsif.traning.loopsDemo;
+
+public class WhileDemo {
+	public static void main(String args[]) {
+		
+		int i = 95; // user value / user input
+		while(i<=100) {
+			System.out.println("value of i: " + i);
+			i++;
+		}
+	}
+
+}
