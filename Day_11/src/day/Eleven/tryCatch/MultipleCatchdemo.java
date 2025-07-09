cd07f0ea05f822053110378c28ebf7f2bfa8b7c7
dipaliugale <ugaledipali969@gmail.com>
2025-07-09 18:03:33 +0530

    day11

---
+++ Day_11/src/day/Eleven/tryCatch/MultipleCatchdemo.java
@@ -0,0 +1,13 @@
+package day.Eleven.tryCatch;
+
+public class MultipleCatchdemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I an a main method");
+		
+		Division.divide();
+
+	}
+
+}
