7e385c7063c35134f092302f6fe5e15f6cc3729b
dipaliugale <ugaledipali969@gmail.com>
2025-07-11 09:23:42 +0530

    day_12

---
+++ Day_11/src/com/tnsif/DayTwelve/ThreadJava/ChildDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.DayTwelve.ThreadJava;
+
+public class ChildDemo {
+
+	public static void main(String[] args) {
+
+
+		ChildThread c = new ChildThread(10, "dipali");
+		ChildThread c1 = new ChildThread(3, "TNSIF");
+		c.start();
+		c1.start();
+		System.out.println("Code is end---");
+
+	}
+
+}
